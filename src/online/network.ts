fadba060847e33b331ed635f0b031f9b5bed01ba
Kinesicpato <283693649+Kinesicpato@users.noreply.github.com>
2026-09-04 15:57:58 -0400

    fix: streamline ICE config with STUN + multiple TURN fallbacks

--- src/online/network.ts
+++ src/online/network.ts
@@ -11,13 +11,8 @@ import {
 } from './types'
 
 const ICE_CONFIG: RTCConfiguration = {
-  iceTransportPolicy: 'all',
   iceServers: [
     { urls: 'stun:stun.l.google.com:19302' },
-    { urls: 'stun:stun1.l.google.com:19302' },
-    { urls: 'stun:stun2.l.google.com:19302' },
-    { urls: 'stun:stun3.l.google.com:19302' },
-    { urls: 'stun:stun4.l.google.com:19302' },
     {
       urls: 'turn:openrelay.metered.ca:80',
       username: 'openrelayproject',
